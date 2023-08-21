3386bbde22d12577e3563de9413fe7b33738aa60
Holly Mathieson <hjmathieson@gmail.com>
2023-08-21 14:47:11 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/app.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header"></header>
+			<h2>Surreal Estate</h2>
+		</div>
+	);
+}
+
+export default App;
