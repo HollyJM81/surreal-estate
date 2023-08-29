4c72658ee282aaba1d3065571616344f7e75f074
Holly Mathieson <hjmathieson@gmail.com>
2023-08-28 21:41:56 -1000

    build addProperties component

--- src/components/AddProperty.js
+++ src/components/AddProperty.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/add-property.css";
 
 const AddProperty = () => {
-	return <div>Add Property Page</div>;
+	const initialState = {
+		fields: {
+			title: "",
+		},
+	};
+
+	const [fields, setFields] = useState(initialState.fields);
+	const handleAddProperty = (event) => {
+		event.preventDefault();
+		console.log(fields);
+	};
+
+	const handleFieldChange = (event) => {
+		setFields({ ...fields, [event.target.name]: event.target.value });
+	};
+
+	return (
+		<div className="add-property">
+			<form onSubmit={handleAddProperty}>
+				<label htmlFor="title">
+					Title
+					<input
+						id="title"
+						name="title"
+						value="fields.title"
+						onChange={handleFieldChange}
+					/>
+				</label>
+				<button type="submit">Add</button>
+			</form>
+		</div>
+	);
 };
 
 export default AddProperty;
